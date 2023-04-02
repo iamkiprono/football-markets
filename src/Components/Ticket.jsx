f8f307f916317192b5bef03fd04ffe1858c689c9
Collins Kiprono Kirui <101986591+iamkiprono@users.noreply.github.com>
2023-04-02 18:37:05 +0300

    Updates

--- src/Components/Ticket.jsx
+++ src/Components/Ticket.jsx
@@ -3,20 +3,18 @@ import "../Components/Ticket.css";
 
 const Ticket = ({ title, date, price, odds, games }) => {
   return (
-    <div>
-      <div className="ticket-wrapper">
-        <h2>{title}</h2>
-        <p>
-          <i>Price: {price}/=</i>
-        </p>
-        <p>Date: {date}</p>
-        <h2>Odds: {odds}</h2>
-        <h4>No of games: {games}</h4>
-        <h4>TILL NO: XXXXXX</h4>
-        <a target="_blank" href="https://wa.me/+254700000000">
-          Buy ticket now!!
-        </a>
-      </div>
+    <div className="ticket-wrapper">
+      <h2>{title}</h2>
+      <p>
+        <i>Price: {price}/=</i>
+      </p>
+      <p>Date: {date}</p>
+      <h2>Odds: {odds}</h2>
+      <h4>No of games: {games}</h4>
+      <h4>TILL NO: XXXXXX</h4>
+      <a target="_blank" href="https://wa.me/+254700000000">
+        Buy ticket now!!
+      </a>
     </div>
   );
 };
